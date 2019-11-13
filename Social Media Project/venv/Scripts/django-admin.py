16a6a624b6abf9237ef085ea8609f2b5e9245e4e
MilanTheGamer <56481574+MilanTheGamer@users.noreply.github.com>
2019-11-13 19:22:15 +0530

    first commit

---
+++ Social Media Project/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\AW\Desktop\Python works\Django\Social Media Project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
